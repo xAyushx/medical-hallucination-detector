7625718a5d2a81f590738473e1f0c9ac5f813cf6
Ayush Narwal <ayushnr09@gmail.com>
2026-07-13 00:18:53 +0530

    made it so that works for 150k sentences

--- src/retriever.py
+++ src/retriever.py
@@ -5,9 +5,10 @@ embed_model = SentenceTransformer("NeuML/pubmedbert-base-embeddings")
 
 client = chromadb.PersistentClient(path="./chroma_db")
 
-collection = client.get_collection("pubmed_corpus")
-
-
+collection = client.get_or_create_collection(
+    name="pubmed_corpus",
+    metadata={"hnsw:space": "cosine"}
+)
 def retrieve(claims, k=3):
     embeddings = embed_model.encode(claims, normalize_embeddings=True).tolist()
     return collection.query(
